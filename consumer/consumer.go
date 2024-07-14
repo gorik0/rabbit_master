977c918fb6eec3c5e4e7e335b96108b4730898e6
gorik0 <delavarus@gmail.com>
2024-07-14 11:51:45 +0300

    RPC_setup

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"github.com/rabbitmq/amqp091-go"
 	"golang.org/x/sync/errgroup"
 	"log"
 	"rabbit-master/internal"
@@ -15,53 +16,73 @@ func HandlerErr(err error, message string) {
 }
 
 func main() {
-	conn, err := (internal.NewRabbiConnection())
-	HandlerErr(err, "connection error")
-	client, err := internal.NewRabbitClient(conn)
-	HandlerErr(err, "client error")
-	defer client.Close()
 
-	//:::; CREATE QUEUE ::::
+	panamaChannel := make(chan struct{})
+	//:: CONN for consume
+	connConsume, err := internal.NewRabbiConnection()
+	HandlerErr(err, "error to create connection")
 
-	queue, err := client.CreateQueue("", true, true)
-	HandlerErr(err, "create queue error")
+	clientConsume, err := internal.NewRabbitClient(connConsume)
+	HandlerErr(err, "error to create client")
 
-	//:::; CREATE BINDING (with no key, in case of fanout  exch-type) ::::
+	defer clientConsume.Close()
 
-	err = client.QueueBind(queue.Name, "", "army_event")
-	HandlerErr(err, "create binding error")
+	queue, err := clientConsume.CreateQueue("", true, false)
+	HandlerErr(clientConsume.QueueBind(queue.Name, "", "army_event"), "queue1 binding error")
 
-	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, time.Second*15)
+	//:: CONN for recieve response
+
+	connSendResp, err := internal.NewRabbiConnection()
+	HandlerErr(err, "error to create connection")
+
+	clientSendResp, err := internal.NewRabbitClient(connSendResp)
+	HandlerErr(err, "error to create client")
+
+	defer clientSendResp.Close()
+
+	//::::: WORK on consuming and sennding response
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
-	msgBus, err := client.ConsumeMessages(ctx, queue.Name, "commando", false)
-	HandlerErr(err, "message bus error")
 
+	ctxResp := context.Background()
+	msgBus, err := clientConsume.ConsumeMessages(ctx, queue.Name, "consumer", false)
+	HandlerErr(err, "error to create consumer")
 	g, ctx := errgroup.WithContext(ctx)
-
-	cha := make(chan struct{})
-	g.SetLimit(10)
-
 	go func() {
 
-		for mes := range msgBus {
+		println("start to consume. ... ")
+		for mesg := range msgBus {
+			println("start another go . ....")
+			println(len(msgBus))
+			msg := mesg
 			g.Go(func() error {
 
-				log.Println("Received message: ", string(mes.Body))
-				time.Sleep(time.Second * 10)
-				err := mes.Ack(false)
-				if err != nil {
-					log.Println("Error acknowledging message: ", err)
-					return err
-				}
-				log.Println("Acknowledged message: ")
-				return nil
-			})
-		}
+				log.Println("GOtta --->> ", string(msg.Body))
 
+				time.Sleep(time.Second * 1)
+				err2 := msg.Ack(false)
+				//log.Println(err2)
+				HandlerErr(err2, "error to ack message")
+
+				//	send respond to
+
+				err2 = clientSendResp.SendMessage(ctxResp, "army_callback", msg.ReplyTo, amqp091.Publishing{
+					ContentType:   "text/plain",
+					DeliveryMode:  amqp091.Transient,
+					CorrelationId: msg.CorrelationId,
+				})
+				if err2 != nil {
+					println("error akcowledged mes")
+
+				}
+				println("end of go")
+				return nil
+
+			},
+			)
+
+		}
 	}()
-	<-cha
-	//client.CreateQueue("private_q", true, false)
-	//HandlerErr(client.QueueBind("private_q", "private.*", "army_event"), "queue1 binding error")
-	//HandlerErr(client.QueueBind("private_q", "private.seaman.*", "army_event"), "queue1 binding error")
+	<-panamaChannel
 }
--- producer/producer.go
+++ producer/producer.go
@@ -5,41 +5,83 @@ import (
 	rabbit "github.com/rabbitmq/amqp091-go"
 	"log"
 	"rabbit-master/internal"
+	"strconv"
 	"time"
 )
 
-func main() {
-	conn, err := internal.NewRabbiConnection()
-	if err != nil {
-		panic(err)
-	}
-	client, err := internal.NewRabbitClient(conn)
-	if err != nil {
-		panic(err)
-	}
-	defer client.Close()
-	//client.CreateQueue("private_q", true, false)
-	//HandlerErr(client.QueueBind("private_q", "private.*", "army_event"), "queue1 binding error")
-	//HandlerErr(client.QueueBind("private_q", "private.seaman.*", "army_event"), "queue1 binding error")
+//:::: RPC *2 connection both on consumer and producer*
+//::: 			kind of exchange - direct
 
+func main() {
+
+	panamaChannel := make(chan struct{})
+
+	//:: CONN for produce
+	err, clientSend := setupSendClient()
+	defer clientSend.Close()
+
+	//:: CONN for recieve response
+
+	clientRcv := setupRcvResponseClient(err)
+
+	defer clientRcv.Close()
+
+	//:::: WORK On RCV response
+
+	//: queue & bind
+	queue, err := clientRcv.CreateQueue("", true, false)
+	HandlerErr(clientRcv.QueueBind(queue.Name, queue.Name, "army_callback"), "queue1 binding error")
+
+	//: consume msg
+	ctx2 := context.Background()
+	msgBus, err := clientRcv.ConsumeMessages(ctx2, queue.Name, "consume_response", true)
+	go func() {
+		for d := range msgBus {
+			log.Printf("Response to : %s", d.CorrelationId)
+		}
+	}()
+
+	//:::: WORK On SEND
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 	for i := 0; i < 10; i++ {
 
-		err := client.SendMessage(ctx, "army_event", "private.vasya", rabbit.Publishing{
-			ContentType:  "text/plain",
-			Body:         []byte("Hello World"),
-			DeliveryMode: rabbit.Transient,
+		err := clientSend.SendMessage(ctx, "army_event", "army_created", rabbit.Publishing{
+			ContentType:   "text/plain",
+			Body:          []byte("Hello World"),
+			DeliveryMode:  rabbit.Persistent,
+			ReplyTo:       queue.Name,
+			CorrelationId: strconv.Itoa(i),
 		})
 		if err != nil {
+
 			HandlerErr(err, "Error while send messs")
 		}
 
 	}
 
+	<-panamaChannel
 	time.Sleep(10 * time.Second)
 
+}
+
+func setupRcvResponseClient(err error) *internal.RabbitClient {
+	connRcv, err := internal.NewRabbiConnection()
+	HandlerErr(err, "error to create connection")
+
+	clientRcv, err := internal.NewRabbitClient(connRcv)
+	HandlerErr(err, "error to create client")
+	return clientRcv
+}
+
+func setupSendClient() (error, *internal.RabbitClient) {
+	connSend, err := internal.NewRabbiConnection()
+	HandlerErr(err, "error to create connection")
+
+	clientSend, err := internal.NewRabbitClient(connSend)
+	HandlerErr(err, "error to create client")
+	return err, clientSend
 }
 
 func HandlerErr(err error, message string) {
